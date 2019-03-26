b932d105faaa9a1e95b1f8cbe693e90f75ef6cc3
Ryan L. Boyd <ryanb@ryanb.cc>
2019-03-26 08:40:19 -0500

    adding py3 scripts

---
+++ CSV to Text - 20000 Files in Subfolders - py3.py
@@ -0,0 +1,78 @@
+'''
+
+  .o oOOOOOOOo                                            OOOo
+   Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+   OboO"""""""""""".OOo. .oOOOOOo.    OOOo.oOOOOOo.."""""""""'OO
+   OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   `"OOOOOOOOOP,OOOOOOOOOOOB'
+   `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo
+   .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO
+   OOOOO                 '"OOOOOOOOOOOOOOOO"`                oOO
+  oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+ oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+OOOO"       "YOoOOOOMOIONODOO"`  .   '"OOROAOPOEOOOoOY"     "OOO"
+   Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`
+   :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?         .
+   .            oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                     `$"  `OOOO' `O"Y ' `OOOO'  o             .
+   .                  .     OP"          : o     .
+                            :
+
+        CSV to Text Files Script
+        Coded by Ryan L. Boyd
+        The University of Texas at Austin
+
+*This script was built for Python 3.x
+*This script is standalone, and should not need additional packages
+'''
+
+import csv
+import os
+
+#what file would you like to input?
+InputFile = "C:/Dataset.csv"
+
+#in what directory would you like to dump the output
+OutputDir = "C:/Txt_Files"
+
+
+
+if OutputDir.endswith("/") == False:
+        OutputDir += "/"
+if not os.path.exists(OutputDir):
+    os.makedirs(OutputDir)
+
+FileNumber = 0
+FolderNumber = '0'
+
+#depending on your input file, you might need to add an encoding
+#such as 'utf-8' after the 'rb'
+with open(InputFile, 'r', encoding='utf8') as file_in:
+    #change the "delimiter" argument to whatever suits your file
+    csvreader = csv.reader(file_in)
+
+
+    HeaderRow = csvreader.__next__()
+    for counter in range(0, len(HeaderRow)):
+        print(str(counter) + ": " + HeaderRow[counter])
+
+    Filename_Var = int(raw_input("Variable to use as file name (use the corresponding number): "))
+    Text_Var = int(raw_input("Variable to use as file text (use the corresponding number): "))
+
+    
+    for row in csvreader:
+
+        if FileNumber % 100000 == 0:
+            FolderNumber = str(FileNumber)
+            if not os.path.exists(OutputDir + FolderNumber):
+                os.makedirs(OutputDir + FolderNumber)
+        FileNumber += 1
+        
+
+        Filename_Clean = row[Filename_Var] + ';' + str(FileNumber)
+        print("Writing file: " + Filename_Clean)
+        #depending on your input file, you might need to add an encoding
+        #such as 'utf-8' after the 'rb'
+        with open(OutputDir + str(FolderNumber) + '/' + Filename_Clean + ".txt", 'w', encoding='utf8') as OutputFile:
+            OutputFile.write(row[Text_Var] + "\n")
